c4c81805cd589aeb6b9b250423d63b6503740209
miurakaikaito <56814146+miurakaikaito@users.noreply.github.com>
2020-07-27 16:51:58 +0900

    マイグレーションのクラス名変更,down()メソッドの記述追加

--- database/migrations/2020_07_27_074200_softdeletes_todos_table.php
+++ database/migrations/2020_07_27_074200_softdeletes_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTodosTable extends Migration
+class SoftdeletesTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -26,7 +26,7 @@ class CreateTodosTable extends Migration
     public function down()
     {
         Schema::table('todos', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 }
