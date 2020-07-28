ba618ea0ce298f8705bcbc786bbd1d2e926833b4
miurakaikaito <56814146+miurakaikaito@users.noreply.github.com>
2020-07-28 11:32:04 +0900

    論理削除のマイグレーションファイル名変更

--- database/migrations/2020_07_28_022645_update_softdeletes_todos_table.php
+++ database/migrations/2020_07_28_022645_update_softdeletes_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SoftdeletesTodosTable extends Migration
+class UpdateSoftdeletesTodosTable extends Migration
 {
     /**
      * Run the migrations.
